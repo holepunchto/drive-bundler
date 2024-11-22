56811aa1293218ad2e0b3adc9c10b3e6fa07cb10
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-11-22 12:06:46 +0100

    Handle relative addon specifiers

--- index.js
+++ index.js
@@ -205,7 +205,7 @@ module.exports = class DriveBundle {
     for (const addon of await Promise.all(addonsPending)) {
       if (!addon) continue
 
-      const dir = this._resolutionKey(addon.input, true)
+      const dir = this._resolutionKey(path.isAbsolute(addon.input) ? addon.input : path.join(path.dirname(addon.referrer), addon.input), true)
       let r = resolutions[dir] = resolutions[dir] || {}
       r['bare:addon'] = addon.output
 
